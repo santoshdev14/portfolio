0a868ac64c5d41c46bbc9210f6ab4d83dd0bff6c
Santosh Varma <Santoshv@SAAR.COM>
2026-08-26 11:39:04 +0530

    Allowed client url

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3000',
-  'https://portfolio-c5hh.onrender.com',
+  'https://santoshvarma.vercel.app',
 
   // Your Vercel frontend
   process.env.FRONTEND_URL,
