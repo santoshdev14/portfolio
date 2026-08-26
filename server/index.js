655248edf9c1c63f02e54bf8ddabf9c5e30bee62
Santosh Varma <Santoshv@SAAR.COM>
2026-08-26 11:31:36 +0530

    allowed new url in server

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3000',
+  'https://portfolio-c5hh.onrender.com',
 
   // Your Vercel frontend
   process.env.FRONTEND_URL,
